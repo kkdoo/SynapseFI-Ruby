04678616f0813b101d7bb7845b8627b4fbfb1a08
Hendra Gunawan <hendragunz@codecampz.com>
2023-01-24 21:03:36 +0700

    more changes to support create Batchtransaction and grouped with Batch ID

--- lib/synapse_pay_rest/models/transaction/batch_transaction.rb
+++ lib/synapse_pay_rest/models/transaction/batch_transaction.rb
@@ -51,9 +51,49 @@ module SynapsePayRest
 
       private
 
+      def payload_for_create_transaction(to_type:, to_id:, amount:, currency:, ip:, **options)
+        payload = {
+          'to' => {
+            'type' => to_type,
+            'id' => to_id
+          },
+          'amount' => {
+            'amount' => amount,
+            'currency' => currency
+          },
+          'extra' => {
+            'ip' => ip
+          }
+        }
+        # optional payload fields
+        payload['extra']['asset']      = options[:asset]      if options[:asset]
+        payload['extra']['same_day']   = options[:same_day]   if options[:same_day]
+        payload['extra']['supp_id']    = options[:supp_id]    if options[:supp_id]
+        payload['extra']['note']       = options[:note]       if options[:note]
+        payload['extra']['process_on'] = options[:process_in] if options[:process_in]
+        payload['extra']['group_id']   = options[:group_id]   if options[:group_id]
+        other = {}
+        other['attachments'] = options[:attachments] if options[:attachments]
+        payload['extra']['other'] = other if other.any?
+        fees = []
+        # deprecated fee flow
+        fee = {}
+        fee['fee']  = options[:fee_amount] if options[:fee_amount]
+        fee['note'] = options[:fee_note] if options[:fee_note]
+        fee_to = {}
+        fee_to['id'] = options[:fee_to_id] if options[:fee_to_id]
+        fee['to'] = fee_to if fee_to.any?
+        fees << fee if fee.any?
+        # new fee flow
+        fees = options[:fees] if options[:fees]
+        payload['fees'] = fees if fees.any?
+        payload
+      end
+
+
       def payload_for_batch_create(transactions:, **options)
         {
-          'transactions' => transactions.map{|transaction| payload_for_create(**transaction)}
+          'transactions' => transactions.map{|transaction| payload_for_create_transaction(**transaction)}
         }
       end
 
